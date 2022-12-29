1bcd205556ca68efeea5af016a5b42378832d8f5
mjkallif <setmowerset@gmail.com>
2022-12-29 12:33:59 +0300

    fdg

--- nginx/static/js/fun.js
+++ nginx/static/js/fun.js
@@ -1,7 +1,20 @@
-let sor_typ = document.querySelector('.rss')
-console.log(sor_typ.style)
+let rss = 1
+let tg = 2
+let openne = 3
+let rbk = 4
+let ria = 5
+let rt = 6
+let colonel = 7
+let interfuck = 1
+let tpraym = 2
+const fromdata = new FormData();
+
 function kok(click_id){
-    console.log(click_id)
+    if (click_id = 'RSS')
+        if (rss === 1){
+            rss = 0;
+        }
+        else rss = 1;
 }
 
 
